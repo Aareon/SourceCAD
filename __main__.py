0d380929b1a09ef13914040ace3d93b8b50e3840
Aareon Sullivan <askully13@gmail.com>
2018-02-12 15:37:56 -0800

    Functioning login and database actions

--- __main__.py
+++ __main__.py
@@ -1,33 +1,72 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+import logging
+from os import urandom
+import sys
+from bcrypt import gensalt, hashpw
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from utils.database import Database, User
 
 # create a Flask application with name "__main__"
+logging.info('Creating `app`')
+db = Database()
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+
+@app.route('/')
+@app.route('/index', methods=['GET'])
 def index():
     """Role selection page"""
-    # if user is logged in, render role selection page 
-    if session.get("logged_in", False):
-        return render_template("index.html",
-                            user_rank=_USER_RANK,
-                            user_name=_USER_NAME,
-                            user_unit_number=_USER_UNIT_NUMBER)
-    # if user *is not* logged in, redirect to login page
+    # if user is logged in, render role selection page
+    if session.get('logged_in'):
+        email, username, unit_number, rank, is_civilian, is_dispatch, is_police, is_admin = db.get_user_info(session['user_id'])
+        return render_template('index.html',
+                               user_rank=rank,
+                               user_name=username,
+                               user_unit_number=unit_number)
     else:
-        print("Not logged in... redirecting to login")
-        return redirect(url_for("login"))
+        return redirect(url_for('login'))
 
-@app.route("/login", methods=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     """Login page"""
-    # if user is not logged in, render login page
-    if not session.get("logged_in", False):
-        return render_template("login.html")
-    #if user *is* logged in, redirect to index (aka the role selection page)
-    else:
-        print("Logged in... redirecting to index")
-        return redirect(url_for("index"))
+    if request.method == 'POST':
+        # get unit number/email field
+        units = request.form.get('units')
+        if units is None:
+            return index()
 
-if __name__ == "__main__":
+        # figure out if the user used their email or unit code to login
+        use_email = False
+        use_unit_number = False
+        if '@' in units:
+            use_email = True
+        else:
+            use_unit_number = True
+
+        # get password, send them back if they didn't put anything
+        password = request.form.get('password')
+        if password is None:
+            return index()
+
+        # get old password hash and salt for a given user from database
+        id, pass_hash, pass_salt = db.get_password(units, use_email=use_email, use_unit_number=use_unit_number)
+
+        # in case a specific user doesn't exist
+        if id is None:
+            return index()
+
+        # encode password for hashing
+        password = password.encode('utf-8')
+        # hash input password with the old salt, if the result matches the old hash, login
+        if hashpw(password, pass_salt) == pass_hash:
+            session['user_id'] = id
+            session['logged_in'] = True
+
+        # complete login... or not.
+        return redirect(url_for('index'))
+
+    else:
+        return render_template('login.html')
+
+if __name__ == '__main__':
+    app.config['SECRET_KEY'] = urandom(24)
     app.run()
--- utils/database.py
+++ utils/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import bcrypt
-import psycopg2
+import sqlite3
 
 Base = declarative_base()
 
@@ -15,9 +15,12 @@ class User(Base):
     username = Column(String(length=32), unique=True, nullable=True)
     pass_hash = Column(String(length=255), unique=True, nullable=False)
     pass_salt = Column(String(length=255), unique=True, nullable=False)
-    is_civilian = Column(Boolean, unique=False, default=False)
-    is_dispatch = Column(Boolean, unique=False, default=False)
-    is_police = Column(Boolean, unique=False, default=False)
+    unit_number = Column(String(length=4), unique=True, nullable=True, default='')
+    rank = Column(String(length=54), unique=False, nullable=True, default='')
+    is_civilian = Column(Integer, unique=False, default=0)
+    is_dispatch = Column(Integer, unique=False, default=0)
+    is_police = Column(Integer, unique=False, default=0)
+    is_admin = Column(Integer, unique=False, default=0)
 
 
     def __repr__(self):
@@ -33,8 +36,8 @@ class User(Base):
 class Character(Base):
     __tablename__ = 'characters'
 
-    id = Column(Integer, ForeignKey('users.id'))
-    name = Column(String(length=255), primary_key=True, nullable=True)
+    id = Column(Integer, ForeignKey('users.id'), primary_key=True, nullable=False)
+    name = Column(String(length=255), nullable=True)
     age = Column(Integer, unique=False, nullable=False)
     address = Column(String(length=255), default='Homeless')
     drivers_license = Column(Boolean, default=True, nullable=False)
@@ -53,7 +56,7 @@ class Character(Base):
 class Registration(Base):
     __tablename__ = 'registrations'
 
-    id = Column(Integer, ForeignKey('users.id'))
+    id = Column(Integer, ForeignKey('users.id'), primary_key=True, nullable=False)
     owner = Column(ForeignKey('characters.id'))
     make = Column(String(32), nullable=False)
     model = Column(String(32), nullable=False)
@@ -69,8 +72,117 @@ class Registration(Base):
                                                     self.tag_number,
                                                     self.insurance)
 
+class Bolo(Base):
+    __tablename__ = 'bolos'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    author = Column(ForeignKey('characters.name'), nullable=False)
+    reason = Column(String(length=140), nullable=True)
+    last_seen = Column(String(length=255), nullable=True)
+    description = Column(String(length=255), nullable=True)
+    notes = Column(String(length=255), nullable=True)
+
+    def __repr__(self):
+        return '<BOLO(id={0}, author=\'{1}\', reason=\'{2}\', last_seen=\'{3}\', \
+        description=\'{4}\', notes=\'{5}\')>'.format(self.id,
+                                                     self.author,
+                                                     self.reason,
+                                                     self.last_seen,
+                                                     self.description,
+                                                     self.notes)
+
+class Ticket(Base):
+    __tablename__ = 'tickets'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    author = Column(ForeignKey('users.id'), nullable=False)
+    offender = Column(ForeignKey('users.id'), nullable=False)
+    penal_code = Column(String(length=16), nullable=False)
+    punishment = Column(String(length=32), nullable=True) # nullable in case its something like a fix-it ticket
+    notes = Column(String(length=140), nullable=True)
+
+    def __repr__(self):
+        return '<Ticket(id={0}, author=\'{1}\', offender=\'{2}\', \
+        penal_code=\'{3}\', punishment=\'{4}\', notes=\'{5}\')>'.format(self.id,
+                                                                        self.author,
+                                                                        self.offender,
+                                                                        self.penal_code,
+                                                                        self.punishment,
+                                                                        self.notes)
+
+class Activity(Base):
+    __tablename__ = 'activities'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    unit = Column(ForeignKey('users.id'), nullable=False)
+    code = Column(String(length=255), nullable=False)
+
+    def __repr__(self):
+        return '<Activity(id={0}, unit=\'{1}\', code=\'{2}\')>'.format(self.id,
+                                                                       self.unit,
+                                                                       self.code)
+
+class Callout(Base):
+    __tablename__ = 'callouts'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    code = Column(String(length=255), nullable=False)
+    reason = Column(String(length=255), nullable=True)
+    location = Column(String(length=255), nullable=True)
+    present_units = Column(String(length=255), nullable=True)
+
+    def __repr__(self):
+        return '<Callout(id={0}, code=\'{1}\', reason=\'{2}\', location=\'{3}\', \
+        present_units=\'{4}\')>'.format(self.id,
+                                        self.code,
+                                        self.reason,
+                                        self.location,
+                                        self.present_units)
+
 class Database:
     def __init__(self):
-        self.engine = create_engine('sqlite+sqlite3:///sourcecad.db?check_same_thread=False', echo=False)
+        self.engine = create_engine('sqlite:///sourcecad.db?check_same_thread=False', echo=False)
         self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
         Base.metadata.create_all(self.engine)
+
+    def get_password(self, item, use_email=False, use_unit_number=False):
+        # if neither methods are specified
+        if not use_email and not use_unit_number:
+            return
+
+        # protect against bots? trying to do more than one thing at once, kind of
+        if use_email and use_unit_number:
+            raise SyntaxError('Too many things to select. Choose email or unit_number, not both.')
+
+        try:
+            # get user data using email
+            if use_email:
+                id, pass_hash, pass_salt = self.session.query(User.id, User.pass_hash, User.pass_salt).filter_by(email=item).all()[0]
+
+            # get user data using unit number
+            if use_unit_number:
+                id, pass_hash, pass_salt = self.session.query(User.id, User.pass_hash, User.pass_salt).filter_by(unit_number=item).all()[0]
+
+            # return the id as well as the encoded password hash and salt ready for hashing and verifying
+            return (id, pass_hash.encode('utf-8'), pass_salt.encode('utf-8'))
+        except:
+            # something happened. Oh well
+            return (None, None, None)
+
+    def get_user_info(self, id=None):
+        # start integrating errors
+        if id is None:
+            return {'errors': 'Invalid session. Try logging back in'}
+
+        try:
+            return self.session.query(User.email,
+                                      User.username,
+                                      User.unit_number,
+                                      User.rank,
+                                      User.is_civilian,
+                                      User.is_dispatch,
+                                      User.is_police,
+                                      User.is_admin).filter_by(id=id).all()[0]
+        except:
+            return (None, None, None, None, None, None, None, None, None)
