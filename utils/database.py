d4e7a4995cd533eee1d63956c634959069326bd2
Aareon Sullivan <askully13@gmail.com>
2018-03-07 13:22:09 -0800

    Remove bcrypt dependency

--- utils/database.py
+++ utils/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import bcrypt
 import sqlite3
 
 Base = declarative_base()
